ad7c89dd536a46f4d1bec4fb511206c49f537689
stephen1521 <stephen.warnock@codeimmersives.com>
2022-10-24 15:43:49 -0400

    updating files

---
+++ main.js
@@ -0,0 +1,26 @@
+let para = document.querySelector('p');
+para.style.color = 'lightblue';
+
+let header = document.querySelector('h1');
+header.style.fontSize = '48px';
+
+para.innerText = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.';
+
+let item13 = document.querySelector('#item-13');
+item13.style.opacity = '0.5';
+
+let item3 = document.querySelector('#item-3');
+item3.innerText = 'Hi!';
+
+let image = document.querySelector('img');
+image.src = 'https://media.istockphoto.com/photos/forest-wooden-table-background-summer-sunny-meadow-with-green-grass-picture-id1353553203?b=1&k=20&m=1353553203&s=170667a&w=0&h=QTyTGI9tWQluIlkmwW0s7Q4z7R_IT8egpzzHjW3cSas=';
+image.style.height = '300px';
+
+let image2 = document.querySelector('#image-2');
+image2.style.height = '300px';
+
+let item16 = document.querySelector('#item-16');
+item16.className = 'item';
+item16.innerText = 'Won\'t get fooled again.';
+
+
